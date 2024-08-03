d51a7a571f855450b53bc42f8dd964d48777c257
elia renzoni <elia.renzoni03@gmail.com>
2024-08-03 08:50:34 +0200

    trimmed sources

--- nodes/node1/ANode.java
+++ nodes/node1/ANode.java
@@ -5,39 +5,25 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 
 // node A must send few messages to node B and C
-// each node must receive 5 messages from node A
 
 public class ANode {
     private static final int LISTEN_PORT = 3030;
     private static final int THREADS = 200;
     public static void main(String... args) {
-        LamportServerSender[] senders = new LamportServerSender[10];
+        LamportServerSender[] senders = new LamportServerSender[2];
         ANodeServerListener lamport = new ANodeServerListener(LISTEN_PORT, THREADS);
         // Node A is now listening at the requestss
         lamport.start();
         
-        // Node is now sendind some few messages
-        for (int i = 0; i < senders.length; i++) {
-            if (i < 5) {
-                try {
-                    senders[i] = new LamportServerSender(new Socket(NodeSystem.NODEB.getNodeHost(), NodeSystem.NODEB.getNodePort()), i);
-                } catch (UnknownHostException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            } else {
-                try {
-                    senders[i] = new LamportServerSender(new Socket(NodeSystem.NODEC.getNodeHost(), NodeSystem.NODEC.getNodePort()), i);
-                } catch (UnknownHostException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            senders[i].start();
+        try {
+            senders[0] = new LamportServerSender(new Socket(NodeSystem.NODEB.getNodeHost(), NodeSystem.NODEB.getNodePort()), 0);
+            senders[0].start();
+            senders[1] = new LamportServerSender(new Socket(NodeSystem.NODEC.getNodeHost(), NodeSystem.NODEC.getNodePort()), 1);
+            senders[1].start();
+        } catch (Exception ex) {
+            System.out.println(ex);
         }
-
+        
         try {
             lamport.join();
             for (int i = 0; i < senders.length; i++) {
